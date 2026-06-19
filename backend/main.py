8c8e42f32e4b9cf4039db206de1c92eb12a4b316
Arnav Katkar <arnavsk1206@gmail.com>
2026-06-19 11:38:21 +0530

    added frontend URL

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 # Allow requests from the Vite dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174","http://localhost:5173", "http://localhost:8000"],
+    allow_origins=["http://localhost:5174","http://localhost:5173", "http://localhost:8000","verdant-elf-3a48bb.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
